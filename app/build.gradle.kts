c824b035f4e668d4e9e359fe6e67917afd4366d9
Bảo <126046620+baoDNB@users.noreply.github.com>
2024-05-30 18:35:40 +0700

    19

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit-gtest:1.0.0-alpha02")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     //firebase
@@ -63,6 +63,9 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics:22.0.0")
     implementation("com.google.firebase:firebase-storage:21.0.0")
     implementation("com.google.firebase:firebase-database:21.0.0")
+
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
 
     implementation ("com.github.barteksc:android-pdf-viewer:2.8.2")
 
---
+++ app/src/main/java/com/example/appbookcase/activity/ProfileActivity.java
@@ -0,0 +1,84 @@
+package com.example.appbookcase.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.example.appbookcase.MyApplication;
+import com.example.appbookcase.R;
+import com.example.appbookcase.databinding.ActivityProfileBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class ProfileActivity extends AppCompatActivity {
+    private ActivityProfileBinding binding;
+    private FirebaseAuth firebaseAuth;
+    private static final  String TAG="PROFILE_TAG";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding= ActivityProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        firebaseAuth =FirebaseAuth.getInstance();
+        loadUserInfo();
+        binding.profileEditBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileActivity.this, ProfileEditActivity.class ));
+            }
+        });
+        binding.backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    private void loadUserInfo() {
+        Log.d(TAG, "loadUserInfo: Loading user info...");
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+        reference.child(firebaseAuth.getUid())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        String email = ""+snapshot.child("email").getValue();
+                        String name = ""+snapshot.child("name").getValue();
+                        String profileImage = ""+snapshot.child("profileImage").getValue();
+                        String timestamp = ""+snapshot.child("timestamp").getValue();
+                        String uid= ""+snapshot.child("uid").getValue();
+                        String userType = ""+snapshot.child("userType").getValue();
+
+                        String formattedDate = MyApplication.formatTimestamp(Long.parseLong(timestamp));
+
+                        binding.emailTv.setText(email);
+                        binding.nameTv.setText(name);
+                        binding.memberDateTv.setText(formattedDate);
+                        binding.accountTypeTv.setText(userType);
+
+                        Glide.with(ProfileActivity.this)
+                                .load(profileImage)
+                                .placeholder(R.drawable.ic_person_gray)
+                                .into(binding.profileIv);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+    }
+}
